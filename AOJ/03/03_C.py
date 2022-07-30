205218f7eebe60afd796e54685ba07c1622de267
Ryo <r.matsuda@4nonome.com>
2022-07-30 18:50:43 +0900

    topic-3の4つのファイルについて、空行と自動フォーマットを訂正しました

--- AOJ/03/03_C.py
+++ AOJ/03/03_C.py
@@ -1,11 +1,7 @@
 while True:
-    x,y= map(int,input().split())
+    x, y = map(int, input().split())
     if x == 0 and y == 0:
         break
-    list = [x,y]
+    list = [x, y]
     list.sort()
-    print(list[0],list[1])
-    
-   
-
-
+    print(list[0], list[1])
