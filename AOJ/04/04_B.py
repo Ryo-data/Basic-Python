bab3a8969422f20dabc253e8e38cf8cc2b463f67
Ryo <r.matsuda@4nonome.com>
2022-08-02 22:13:58 +0900

    AOJtopic-4の4つのファイルを変更しました

---
+++ AOJ/04/04_B.py
@@ -0,0 +1,5 @@
+p = float(3.141592653589)
+r = float(input())
+s = (r**2) * p  # 面積
+l = r * 2 * p  # 円周
+print(f"{s:.6f} {l:.6f}")
---
+++ AOJ/04/04_C.py
@@ -0,0 +1,15 @@
+while True:
+    list = input().split()
+    a = int(list[0])
+    op = list[1]
+    b = int(list[2])
+    if op == "?":
+        break
+    if op == "+":
+        print(a + b)
+    if op == "-":
+        print(a - b)
+    if op == "*":
+        print(a * b)
+    if op == "/":
+        print(a // b)
